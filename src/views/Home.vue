b0eabebae608de5cbcef015c581faf9c59b18799
A-Dee <62553246+A-Dee@users.noreply.github.com>
2021-09-21 11:22:41 +0100

    files

---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <h1 class="text-6xl text-center tracking-wider">SELF TAILOR</h1>
+
+<div class="flex justify-center border-t-2 border-b-2 mb-16 ">
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">ARTICLES</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">LEARNING PLATFROM</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">MANNEQUIN MARRA</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">PATTERNS</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">PADS</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">SKETCHBOOK</div>
+  <div class="text-gray-700 no-underline hover:underline text-center  px-4 py-2 m-2">TAPE</div>
+</div>
+
+<section class="mx-32 ">
+   <div class=" mb-12"  >
+    <img class= "h-100%" src="@/assets/BannerImg.jpg" alt="" title="" itemprop="contentURL">
+    </div>
+    <h1 class="text-4xl tracking-wider text-yellow-600 border-width: 2px">PROFESSIONS</h1>
+
+  <div class="md:flex">
+  <div class="md:flex-shrink">
+    <img class="mb-12 " src="@/assets/img2.jpg"  alt="">
+      <div class="mt-4 md:mt-0 md:ml-6">
+    <div class="uppercase tracking-wide text-sm text-yellow-600 font-bold">SEWING CLUB</div>
+  
+    <p class="mt-2 text-gray-600">The club is a community of girls around the world who are passionate about sewing and handicrafts. We have done everything so that you can constantly replenish your knowledge and develop in sewing without stress and deadlines. Nothing extra. Only applied information, sewing life hacks and a cool company.</p>
+  </div>
+  </div>
+
+  <div class ="mt-4 md:mt-0 md:ml-6" >
+   <img class="mb-12  " src="@/assets/img3.jpg"  alt="">
+    <div class="uppercase tracking-wide text-sm text-yellow-600 font-bold">CLUB BY STAND</div>
+  
+    <p class="mt-2 text-gray-600">In this club, we focus on a detailed 3D analysis of forms and learn how to model absolutely any figure of the customer: the most complex and the most remote. The ability to think in volume will allow you to jump through 5-10 years of experience in plane construction. </p>
+  </div>
+</div>
+
+<div class="md:flex">
+  <div class="flex flex-wrap -mx-4">
+    <div class="md:w-1/2 w-auto px-4">
+     <div class="mb-4"><img class="rounded shadow-md" src="@/assets/img4.jpg" alt=""> </div>
+     <div class="mb-4"><img class="rounded shadow-md" src="@/assets/img4.jpg" alt=""> </div>
+     <div class="mb-4"><img class="rounded shadow-md" src="@/assets/img4.jpg" alt=""> </div>
+     <div class="mb-4"><img class="rounded shadow-md" src="@/assets/img4.jpg" alt=""> </div>
+    </div>
+    
+    
+
+  </div>
+  
+
+</div>
+
+</section> 
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
